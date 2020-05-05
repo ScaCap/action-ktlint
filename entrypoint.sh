fc91970e0a060806b00a082738833e4bbfcac5f2
Gustavo Fao <faogustavo@gmail.com>
2020-05-05 08:20:10 -0300

    added pipefail back

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+if [ "$INPUT_FAIL_ON_ERROR" = true ] ; then
+  set -o pipefail
+fi
+
 cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 ktlint --reporter=checkstyle \
-  | reviewdog -f=checkstyle -name="ktlint" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}" -fail-on-error=$INPUT_FAIL_ON_ERROR
+  | reviewdog -f=checkstyle -name="ktlint" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
