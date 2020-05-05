8a86a68ff7bef92a6bcbdc42f8bc61b107406c8a
Gustavo Fao <faogustavo@gmail.com>
2020-05-05 08:02:27 -0300

    add fail on error input to action

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ cd "$GITHUB_WORKSPACE"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 ktlint --reporter=checkstyle \
-  | reviewdog -f=checkstyle -name="ktlint" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
+  | reviewdog -f=checkstyle -name="ktlint" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}" -fail-on-error=$INPUT_FAIL_ON_ERROR
