705c736bd376c8031a02485d020e4eb46e3ca318
dvdandroid <dvdandroid@users.noreply.github.com>
2020-06-12 00:27:12 +0200

    Add Android flag support

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+export ANDROID=
+
 if [ "$INPUT_FAIL_ON_ERROR" = true ] ; then
   set -o pipefail
+fi
+
+if [ "$INPUT_ANDROID" = true ] ; then
+  export ANDROID=--android
 fi
 
 cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-ktlint --reporter=checkstyle \
+ktlint --reporter=checkstyle $ANDROID \
   | reviewdog -f=checkstyle -name="ktlint" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
