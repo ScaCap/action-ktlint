2dfef27ccb6a368c563aa9d0bd1f9635dee26cd0
Renato Abreu <renatoabreu1196@gmail.com>
2020-09-01 20:15:12 +0200

    Move curl

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,20 +5,18 @@ export ANDROID=
 export KTLINT_RELEASE=
 
 if [ "$INPUT_KTLINT_VERSION" = "latest" ] ; then
-  export KTLINT_RELEASE="https://api.github.com/repos/pinterest/ktlint/releases/latest"
-else
-  export KTLINT_RELEASE="https://github.com/pinterest/ktlint/releases/download/${INPUT_KTLINT_VERSION}/ktlint"
-fi
-
-echo ktlint release version: "${KTLINT_RELEASE}"
-
-curl -sSL "${KTLINT_RELEASE}" \
+  curl -sSL https://api.github.com/repos/pinterest/ktlint/releases/latest \
     | grep "browser_download_url.*ktlint\"" \
     | cut -d : -f 2,3 \
     | tr -d \" \
     | wget -qi -\
     && chmod a+x ktlint \
     && mv ktlint /usr/local/bin/
+else
+  curl -sSLO https://github.com/pinterest/ktlint/releases/download/"${INPUT_KTLINT_VERSION}"/ktlint \
+    && chmod a+x ktlint \
+    && mv ktlint /usr/local/bin/
+fi
 
 if [ "$INPUT_RELATIVE" = true ] ; then
   export RELATIVE=--relative
