6559319ebc81a8d4ad943098d0019acf038b53eb
Ghais Zaher <ghaiszaher@gmail.com>
2022-12-24 23:34:33 +0100

    revert debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,17 +5,12 @@ export ANDROID=
 export BASELINE=
 
 if [ "$INPUT_KTLINT_VERSION" = "latest" ]; then
-  echo "Downloading latest..."
-  content=$(curl -L https://api.github.com/repos/pinterest/ktlint/releases/latest --header "authorization: Bearer ${INPUT_GITHUB_TOKEN}")
-  echo "$content"
-  url=$(echo "$content" | grep "browser_download_url.*ktlint\"")
-  echo "url: ${url}"
-  cleaned="$(echo "$url" | cut -d : -f 2,3 | tr -d \")"
-  echo "cleaned: $cleaned"
-  echo "$cleaned" | wget -i -
-  chmod a+x ktlint
-  mv ktlint /usr/local/bin/
-  echo "Done downloading."
+  curl -sSL https://api.github.com/repos/pinterest/ktlint/releases/latest --header "authorization: Bearer ${INPUT_GITHUB_TOKEN}" |
+    grep "browser_download_url.*ktlint\"" |
+    cut -d : -f 2,3 |
+    tr -d \" |
+    wget -qi - && chmod a+x ktlint &&
+    mv ktlint /usr/local/bin/
 else
   curl -sSLO https://github.com/pinterest/ktlint/releases/download/"${INPUT_KTLINT_VERSION}"/ktlint &&
     chmod a+x ktlint &&
