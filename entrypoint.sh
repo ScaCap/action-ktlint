fd5d922e4b9ef7cfa5e6f7c32a2dc85d9df9e9e0
Ghais Zaher <ghaiszaher@gmail.com>
2022-12-24 22:30:47 +0100

    don't silence curl

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ export BASELINE=
 
 if [ "$INPUT_KTLINT_VERSION" = "latest" ]; then
   echo "Downloading latest..."
-  curl -sSL https://api.github.com/repos/pinterest/ktlint/releases/latest |
+  curl -L https://api.github.com/repos/pinterest/ktlint/releases/latest |
     grep "browser_download_url.*ktlint\"" |
     cut -d : -f 2,3 |
     tr -d \" |
@@ -15,7 +15,7 @@ if [ "$INPUT_KTLINT_VERSION" = "latest" ]; then
   mv ktlint /usr/local/bin/
   echo "Done downloading."
 else
-  curl -sSLO https://github.com/pinterest/ktlint/releases/download/"${INPUT_KTLINT_VERSION}"/ktlint &&
+  curl -LO https://github.com/pinterest/ktlint/releases/download/"${INPUT_KTLINT_VERSION}"/ktlint &&
     chmod a+x ktlint &&
     mv ktlint /usr/local/bin/
 fi
