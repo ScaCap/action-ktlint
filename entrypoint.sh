c3bb97bdf506d059ea378acf49d1db60fb5d13e9
Ghais Zaher <ghaiszaher@gmail.com>
2022-12-25 10:27:49 +0100

    reformat

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,8 @@ if [ "$INPUT_KTLINT_VERSION" = "latest" ]; then
     grep "browser_download_url.*ktlint\"" |
     cut -d : -f 2,3 |
     tr -d \" |
-    wget -qi - && chmod a+x ktlint &&
+    wget -qi - &&
+    chmod a+x ktlint &&
     mv ktlint /usr/local/bin/
 else
   curl -sSLO https://github.com/pinterest/ktlint/releases/download/"${INPUT_KTLINT_VERSION}"/ktlint &&
