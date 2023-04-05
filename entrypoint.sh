776e62f70b39f9a3e2272de7877b7fdc4d02deb3
masashi <wf9a5m75@gmail.com>
2023-04-05 00:31:35 -0700

    [test] change the ktlint downloader code

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,15 @@ export ANDROID=
 export BASELINE=
 export CUSTOM_RULE_PATH=
 
+echo "INPUT_KTLINT_VERSION=${INPUT_KTLINT_VERSION}"
 if [ "$INPUT_KTLINT_VERSION" = "latest" ]; then
-  curl -sSL https://api.github.com/repos/pinterest/ktlint/releases/latest --header "authorization: Bearer ${INPUT_GITHUB_TOKEN}" |
-    grep "browser_download_url.*ktlint\"" |
-    cut -d : -f 2,3 |
-    tr -d \" |
-    wget -qi - &&
-    chmod a+x ktlint &&
-    mv ktlint /usr/local/bin/
+  curl -sSL https://api.github.com/repos/pinterest/ktlint/releases/latest \
+    | grep "browser_download_url.*ktlint\"" \
+    | cut -d : -f 2,3 \
+    | tr -d \" \
+    | wget -qi -\
+    && chmod a+x ktlint \
+    && mv ktlint /usr/local/bin/
 else
   curl -sSLO https://github.com/pinterest/ktlint/releases/download/"${INPUT_KTLINT_VERSION}"/ktlint &&
     chmod a+x ktlint &&
